cdad41491125e8da9e0e5530ad5aa4b0e0c4ab1a
TomoeMami <trembleafterme@Outlook.com>
2021-10-29 22:35:06 +0800

    test push

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-from bilibili_api import user, sync
-import json
+import requests,json
 
-u = user.User(703007996)
-async def main():
-    offset = 0
-    page = await u.get_dynamics(offset)
-    with open ('./test.json',"w",encoding='utf-8') as f:
-        f.write(json.dumps(page,indent=2,ensure_ascii=False))
+qqmsg = []
 
-sync(main())
+qqurl = 'http://127.0.0.1:1314'
+qqdata = {'type' : 'ReplyPush','msg':qqmsg}
+qqreq = requests.post(qqurl,json=qqdata)
+
