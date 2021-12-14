2b8f793eef33707f6b368de0d375965e37440344
TomoeMami <trembleafterme@Outlook.com>
2021-12-14 17:47:18 +0800

    调整RebuildCached代码

--- RebuildCache.py
+++ RebuildCache.py
@@ -6,7 +6,7 @@ from bilibili_api import user,sync
 headers = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
 
 idlist= ['672346917','703007996','672353429','672342685','351609538','672328094']
-dylist = {'672342685':'https://www.douyin.com/user/MS4wLjABAAAAxCiIYlaaKaMz_J1QaIAmHGgc3bTerIpgTzZjm0na8w5t2KTPrCz4bm_5M5EMPy92','672353429':'https://www.douyin.com/user/MS4wLjABAAAAlpnJ0bXVDV6BNgbHUYVWnnIagRqeeZyNyXB84JXTqAS5tgGjAtw0ZZkv0KSHYyhP','351609538':'https://www.douyin.com/user/MS4wLjABAAAAuZHC7vwqRhPzdeTb24HS7So91u9ucl9c8JjpOS2CPK-9Kg2D32Sj7-mZYvUCJCya','672346917':'https://www.douyin.com/user/MS4wLjABAAAAxOXMMwlShWjp4DONMwfEEfloRYiC1rXwQ64eydoZ0ORPFVGysZEd4zMt8AjsTbyt','672328094':'https://www.douyin.com/user/MS4wLjABAAAA5ZrIrbgva_HMeHuNn64goOD2XYnk4ItSypgRHlbSh1c','703007996':'https://www.douyin.com/user/MS4wLjABAAAAflgvVQ5O1K4RfgUu3k0A2erAZSK7RsdiqPAvxcObn93x2vk4SKk1eUb6l_D4MX-n'}
+uid_list = {'672342685':'MS4wLjABAAAAxCiIYlaaKaMz_J1QaIAmHGgc3bTerIpgTzZjm0na8w5t2KTPrCz4bm_5M5EMPy92','672353429':'MS4wLjABAAAAlpnJ0bXVDV6BNgbHUYVWnnIagRqeeZyNyXB84JXTqAS5tgGjAtw0ZZkv0KSHYyhP','351609538':'MS4wLjABAAAAuZHC7vwqRhPzdeTb24HS7So91u9ucl9c8JjpOS2CPK-9Kg2D32Sj7-mZYvUCJCya','672346917':'MS4wLjABAAAAxOXMMwlShWjp4DONMwfEEfloRYiC1rXwQ64eydoZ0ORPFVGysZEd4zMt8AjsTbyt','672328094':'MS4wLjABAAAA5ZrIrbgva_HMeHuNn64goOD2XYnk4ItSypgRHlbSh1c','703007996':'MS4wLjABAAAAflgvVQ5O1K4RfgUu3k0A2erAZSK7RsdiqPAvxcObn93x2vk4SKk1eUb6l_D4MX-n'}
 
 with open ('/home/ubuntu/dycookie.txt','r',encoding='utf-8') as f:
         cookie_str1 = f.read()
@@ -29,21 +29,25 @@ async def get_bili(uid,dynamics):
         url = 'https://t.bilibili.com/'+sdata['desc']['dynamic_id_str']
         RssData[str(uid)]['bili'][url]= '发布动态'
 
-async def get_douyin(uid,url):
+async def get_douyin(uid):
+    url = 'https://www.douyin.com/user/'+uid_list[uid]
     async with aiohttp.ClientSession(headers=headers,cookies=cookies) as session:
         async with session.get(url) as response:
             result = await response.content.read()
         res = result.decode('utf-8')
-        dlinks = re.findall(r'https://www.douyin.com/video/\d+',str(result))
+        dlinks = re.findall(r'<a href="(//www\.douyin\.com/video/\d+)"',str(res))
+        dlinks = list(dlinks)
+        for i in range(len(dlinks)):
+            dlinks[i] = 'https:'+dlinks[i]
         for dlink in dlinks:
-            RssData[uid]['douyin'][dlink]='发布抖音'
+            RssData[str(uid)]['douyin'][dlink]='发布抖音'
 
 async def main():
     tasks = []
     dynamics = {"672342685":{},"703007996":{},"672353429":{},"351609538":{},"672346917":{},"672328094":{}}
     for uid in idlist:
         tasks.append(get_bili(uid,dynamics))
-        tasks.append(get_douyin(uid,dylist[uid]))
+        tasks.append(get_douyin(uid)
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
