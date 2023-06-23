dc8dfcbb039f02e3e62a1487fdfdc72fb93a0a70
PushBot <Fake@email.com>
2023-06-23 14:00:26 +0800

    [脚本]自动上传于 2023年06月23日 14:00

--- test.py
+++ test.py
@@ -1,27 +1,10 @@
 import requests,json,re
 
-qqmsg = []
-
-link ="https://t.bilibili.com/587030370470553294"
-
-summary = "[b][url=https://t.bilibili.com/587030370470553294]发布动态[/url][/b]\n[quote]“提前晚安   关心我都看到辣～” [/quote]"
-
-qqmsg.append({"type":"Plain", "text":str(link)})
-
-tempmsg = re.sub(r'\[.+?\]','',summary)
-
-qqmsg.append({"type":"Plain", "text":str(tempmsg)})
-
-qqmsg_img = re.findall(r'\[img\](.*?)\[/img\]',summary)
-
-if(len(qqmsg_img)!=0):
-    for i in qqmsg_img:
-        qqmsg.append({"type":"Image", "url":str(i)})
-print(qqmsg)
-qqurl = 'http://127.0.0.1:7890/sendGroupMessage'
+qqmsg = 'test'
+qqurl = 'http://127.0.0.1:7890/send_msg'
 qqdata = {
-  "sessionKey":"",
-  "target":614391357,
-  "messageChain":qqmsg
+  "message_type": "group",
+  "group_id":822519722,
+  "message":qqmsg
 }
 qqreq = requests.post(qqurl,json=qqdata)
