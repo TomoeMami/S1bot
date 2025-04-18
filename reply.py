37ccdfc4cabb89e58c0eaba0fdcec0dc349fcc83
Anonymous <anonymous@example.com>
2025-04-19 00:09:01 +0800

    [脚本]自动上传于 2025年04月19日 00:09

--- reply.py
+++ reply.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
             # 设置请求头
             headers = {'User-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78'}
             ''' 获取formhash'''
-            RURL = 'https://bbs.saraba1st.com/2b/forum.php?mod=viewthread&tid=334540&extra=page%3D1'
+            RURL = 'https://stage1st.com/2b/forum.php?mod=viewthread&tid=334540&extra=page%3D1'
             s1 = requests.get(RURL, headers=headers,  cookies=cookies)
             content = s1.content
             rows = re.findall(r'<input type=\"hidden\" name=\"formhash\" value=\"(.*?)\" />', str(content)) #正则匹配找到formhash值
@@ -69,7 +69,7 @@ if __name__ == '__main__':
                 '''A综ID，手动修改'''
                 # threadid = 2028372
                 '''A综ID，手动修改'''
-                replyurl = 'https://bbs.saraba1st.com/2b/forum.php?mod=post&action=reply&fid=151&tid='+str(threadid)+'&extra=page%3D1&replysubmit=yes'
+                replyurl = 'https://stage1st.com/2b/forum.php?mod=post&action=reply&fid=151&tid='+str(threadid)+'&extra=page%3D1&replysubmit=yes'
                 #url为要回帖的地址
                 Data = {'formhash': formhash,'message': msg,'subject': subject,'posttime':int(time.time()),'wysiwyg':1,'usesig':1}
                 req = requests.post(replyurl,data=Data,headers=headers,cookies=cookies)
